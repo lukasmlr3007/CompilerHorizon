0098045cafbb66aeca3c49172d53316d206df3df
Dennis Heine <dennis.heine@online.de>
2023-05-17 11:25:18 +0200

    added new Expressions

--- src/main/java/parser/adapter/LogicalExpressionAdapter.java
+++ src/main/java/parser/adapter/LogicalExpressionAdapter.java
@@ -2,11 +2,16 @@ package parser.adapter;
 
 import parser.JavaGrammerParser;
 import syntax.expression.AdditiveExpression;
+import syntax.expression.Expression;
 import syntax.expression.LogicalExpression;
+import syntax.expression.PartExpression;
 
 public class LogicalExpressionAdapter {
 
     public static LogicalExpression adapt(JavaGrammerParser.LogicalExpressionContext logicalExpressionContext){
-        return new LogicalExpression(null, null,null);
+        PartExpression exLeft = PartExpressionAdapter.adapt(logicalExpressionContext.partExpression());
+        Expression exRight = ExpressionAdapter.adapt(logicalExpressionContext.expression());
+        String operator = logicalExpressionContext.LogicOperator().getSymbol().getText();
+        return new LogicalExpression(operator, exLeft,exRight);
     }
 }
