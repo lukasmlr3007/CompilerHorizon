d0ac86268bf3477c831cf7d488cf6ba5510e36d4
Florian Sprenger <61649755+Nairo05@users.noreply.github.com>
2023-06-22 01:11:01 +0200

    test

--- src/test/resources/TAST/MethodAccessModifierTest.java
+++ src/test/resources/TAST/MethodAccessModifierTest.java
@@ -1,22 +1,14 @@
 class MethodAccessModifierTest {
-
     public AnotherClass anotherClass;
-
     public void test() {
         this.anotherClass = new AnotherClass();
         this.anotherClass.tested();
     }
 }
-
 class AnotherClass {
-
     public AnotherClass() {
-
     }
-
     private void tested() {
 
-
     }
-
 }
